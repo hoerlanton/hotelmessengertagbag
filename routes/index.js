595a8dd620ce2f260e57d44811c63eb320a524b0
Anton Hörl <antonhorl@Antons-MacBook-Pro.local>
2017-05-19 19:50:11 +0200

    Buchungsbestätigung abgeändert. BuchungsPDF abgeändert.

--- routes/index.js
+++ routes/index.js
@@ -198,7 +198,7 @@ function sendBookingConfirmation(recipientId, a, b, c, d, e, f, g) {
             id: recipientId
         },
         message: {
-            text: "Hier Ihre Buchungsbestätigung: \nName:" + a + "\nAdresse:" + b + "\nPersonenanzahl:" + c + "\nZimmeranzahl:" + d + "\nAnreisedatum:" + e + "\nAbreisedatum:" + f + "\nAbgebuchter Betrag:" + g,
+            text: "Sehr geehrte/r Frau Herr " + a + "\n Vielen Dank für Ihre Buchung im Hotel Salzburger Hof Leogang. Hiermit senden wir Ihnen die Buchungsbestätigung und freuen und bis bald im Hotel Salzburger Hof Leogang. \n \n Mit Freundlichen Grüßen \n Familie Hörl samt Team \n BeispielBuchungsbestätigung: \n Adresse: " + b + "\nPersonenanzahl: " + c + "\nZimmeranzahl: " + d + "\nAnreisedatum: " + e + "\nAbreisedatum: " + f + "\nAbgebuchter Betrag: " + g",00 EUR",
             metadata: "DEVELOPER_DEFINED_METADATA"
         }
 };
@@ -215,7 +215,7 @@ function sendPDF(recipientId) {
             attachment: {
                 type: "file",
                 payload: {
-                    url: "http://servicio.io/wp-content/uploads/2017/05/Buchungsbestätigung.pdf"
+                    url: "http://servicio.io/wp-content/uploads/2017/05/Buchungsbestätigung-1.pdf"
                 }
             }
         }
