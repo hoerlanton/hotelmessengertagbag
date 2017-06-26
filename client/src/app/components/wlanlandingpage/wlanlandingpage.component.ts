49775de2dc21dfbdbee7395f681a8a90ece33444
Anton Hörl <antonhorl@Antons-MacBook-Pro.local>
2017-06-26 15:19:01 +0200

    Latest changes - First try publishing to Github
    Latest changes - First try publishing to Github

---
+++ client/src/app/components/wlanlandingpage/wlanlandingpage.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wlanlandingpage',
+  templateUrl: './wlanlandingpage.component.html',
+  styleUrls: ['./wlanlandingpage.component.css'],
+})
+export class WlanlandingpageComponent implements OnInit {
+  ngOnInit() {
+  }
+  constructor() {
+  }
+}
+
+
+
+/*
+
+    this.loadScript('facebook.js');
+
+
+
+  public loadScript(url) {
+    console.log('preparing to load...');
+    let node = document.createElement('script');
+    node.src = url;
+    node.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(node);
+  }
+  */
